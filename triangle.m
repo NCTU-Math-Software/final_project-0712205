bf61c619e15e5536e17fa5f6e01217630f1c24d5
yqyang <71300391+0712205@users.noreply.github.com>
2020-12-18 16:13:22 +0800

    Add files via upload

--- triangle.m
+++ triangle.m
@@ -3,28 +3,45 @@ grid
 line([0 0],[-10 10]);
 line([-10 10],[0 0]);
 hold on
+axis equal
+plot(0,0,'ro')
+%給定3個點
 [xx,yy,BUTTON] = ginput(3);
 plot([xx(1) xx(2)], [yy(1) yy(2)], 'r');
 plot([xx(2) xx(3)], [yy(2) yy(3)], 'r');
 plot([xx(1) xx(3)], [yy(1) yy(3)], 'r');
-tt=abs((xx(2)-xx(1))*(yy(3)-yy(1))-(yy(2)-yy(1))*(xx(3)-xx(1)));
-aa=abs(xx(1)*yy(2)-yy(1)*xx(2))+abs(xx(2)*yy(3)-yy(2)*xx(3))+abs(xx(1)*yy(3)-yy(1)*xx(3));
-if t==a
-    disp("yes")
+%ABC面積
+tt=1/2*abs(det([xx(2)-xx(1) yy(2)-yy(1); xx(3)-xx(1) yy(3)-yy(1)]));
+%OA+OB+OC面積
+aa=1/2*(abs(det([xx(1) yy(1); xx(2) yy(2)]))+abs(det([xx(2) yy(2); xx(3) yy(3)]))+abs(det([xx(3) yy(3); xx(1) yy(1)])));
+%若ABC面積=OA+OB+OC面積，O在ABC內
+if tt<=(aa+10^(-5)) && tt>=(aa-10^(-5))
+    disp("The original point is in the triangle.")
 else
-    disp("no")
+    disp("The original point is not in the triangle.")
 end
-x1=[(xx(1)-xx(2)) yy(1)-yy(2)];
-x2=[(xx(3)-xx(2)) yy(3)-yy(2)];
-x3=[(xx(1)-xx(3)) yy(1)-yy(3)];
-ab=norm(x1);
-bc=norm(x2);
-ca=norm(x3);
-int=[(bc*xx(1)+ca*xx(2)+ab*xx(3))./(bc+ca+ab) (bc*yy(1)+ca*yy(2)+ab*yy(3))./(bc+ca+ab)]
-oi=2*aa./(ab+bc+ca);
-alpha=0:pi/10000:2*pi;
-x=oi*cos(alpha)+(bc*xx(1)+ca*xx(2)+ab*xx(3))./(bc+ca+ab);
-y=oi*sin(alpha)+(bc*yy(1)+ca*yy(2)+ab*yy(3))./(bc+ca+ab);
-plot((bc*xx(1)+ca*xx(2)+ab*xx(3))./(bc+ca+ab),(bc*yy(1)+ca*yy(2)+ab*yy(3))./(bc+ca+ab),'r+',x,y);
-axis equal;
+ A=[xx(1) yy(1)];
+ B=[xx(2) yy(2)];
+ C=[xx(3) yy(3)];
+ ab=norm(B-A);
+    bc=norm(B-C);
+    ca=norm(C-A);
+    %內心座標
+    int=[(bc.*xx(1)+ca.*xx(2)+ab.*xx(3))./(bc+ca+ab) (bc.*yy(1)+ca.*yy(2)+ab.*yy(3))./(bc+ca+ab)]
+    %內心半徑
+    oi=2*aa./(ab+bc+ca);
+    %外心座標
+    oo=[(xx(1)^2+(yy(1))^2) yy(1) 1;(xx(2)^2+(yy(2))^2) yy(2) 1;(xx(3)^2+(yy(3))^2) yy(3) 1];
+    ii=[xx(1) (xx(1)^2+(yy(1))^2) 1;xx(2) (xx(2)^2+(yy(2))^2) 1;xx(3) (xx(3)^2+(yy(3))^2) 1];
+    cir=[det(oo)./(4*aa) det(ii)./(4*aa)]
+    oj=(ab*bc*ca)./(4*aa);
+    %畫出內切圓
+    alpha=0:pi/1000:2*pi;
+    x=oi*cos(alpha)+(bc.*xx(1)+ca.*xx(2)+ab.*xx(3))./(bc+ca+ab);
+    y=oi*sin(alpha)+(bc.*yy(1)+ca.*yy(2)+ab.*yy(3))./(bc+ca+ab);
+    plot((bc*xx(1)+ca*xx(2)+ab*xx(3))./(bc+ca+ab),(bc*yy(1)+ca*yy(2)+ab*yy(3))./(bc+ca+ab),'r+',x,y);
+    %畫出外接圓
+    x1=oj*cos(alpha)+cir(1);
+    y1=oj*sin(alpha)+cir(2);
+    plot(cir(1),cir(2),'r+',x1,y1);
 
